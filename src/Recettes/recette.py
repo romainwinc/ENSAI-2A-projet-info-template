bb3554e140c130360201e54bdfa57e22eb0cf429
xlecureuil <braquaval.xavier@gmail.com>
2024-09-17 16:17:18 +0200

    maj Connecte et recette

--- src/Recettes/recette.py
+++ src/Recettes/recette.py
@@ -8,50 +8,58 @@ class Recette:
     composition_chimique : dict[SubstanceChimique, float]
         composition chimique du carburant
     """
-    def __init__(self, idMeal: str, strMeal: str, strCategory: str, 
-                 strArea: str, strInstructions: str, strTags: str | None,
-                 listIngredient: list[Ingredient],
-                 strIngredient1: str | None, 
-                 strIngredient2: str | None, 
-                 strIngredient3: str | None, 
-                 strIngredient4: str | None, 
-                 strIngredient5: str | None, 
-                 strIngredient6: str | None, 
-                 strIngredient7: str | None, 
-                 strIngredient8: str | None, 
-                 strIngredient9: str | None, 
-                 strIngredient10: str | None, 
-                 strIngredient11: str | None, 
-                 strIngredient12: str | None, 
-                 strIngredient13: str | None, 
-                 strIngredient14: str | None, 
-                 strIngredient15: str | None, 
-                 strIngredient16: str | None, 
-                 strIngredient17: str | None, 
-                 strIngredient18: str | None, 
-                 strIngredient19: str | None, 
-                 strIngredient20: str | None, 
-                 strMeasure1: str | None, 
-                 strMeasure2: str | None, 
-                 strMeasure3: str | None, 
-                 strMeasure4: str | None, 
-                 strMeasure5: str | None, 
-                 strMeasure6: str | None, 
-                 strMeasure7: str | None, 
-                 strMeasure8: str | None, 
-                 strMeasure9: str | None, 
-                 strMeasure10: str | None, 
-                 strMeasure11: str | None, 
-                 strMeasure12: str | None, 
-                 strMeasure13: str | None, 
-                 strMeasure14: str | None, 
-                 strMeasure15: str | None, 
-                 strMeasure16: str | None, 
-                 strMeasure17: str | None, 
-                 strMeasure18: str | None, 
-                 strMeasure19: str | None,
-                 strMeasure20: str | None) -> None:
-        
+
+    def __init__(
+        self,
+        idMeal: str,
+        strMeal: str,
+        strCategory: str,
+        strArea: str,
+        strInstructions: str,
+        strTags: str | None,
+        listIngredient: list[Ingredient],
+        strIngredient1: str | None,
+        strIngredient2: str | None,
+        strIngredient3: str | None,
+        strIngredient4: str | None,
+        strIngredient5: str | None,
+        strIngredient6: str | None,
+        strIngredient7: str | None,
+        strIngredient8: str | None,
+        strIngredient9: str | None,
+        strIngredient10: str | None,
+        strIngredient11: str | None,
+        strIngredient12: str | None,
+        strIngredient13: str | None,
+        strIngredient14: str | None,
+        strIngredient15: str | None,
+        strIngredient16: str | None,
+        strIngredient17: str | None,
+        strIngredient18: str | None,
+        strIngredient19: str | None,
+        strIngredient20: str | None,
+        strMeasure1: str | None,
+        strMeasure2: str | None,
+        strMeasure3: str | None,
+        strMeasure4: str | None,
+        strMeasure5: str | None,
+        strMeasure6: str | None,
+        strMeasure7: str | None,
+        strMeasure8: str | None,
+        strMeasure9: str | None,
+        strMeasure10: str | None,
+        strMeasure11: str | None,
+        strMeasure12: str | None,
+        strMeasure13: str | None,
+        strMeasure14: str | None,
+        strMeasure15: str | None,
+        strMeasure16: str | None,
+        strMeasure17: str | None,
+        strMeasure18: str | None,
+        strMeasure19: str | None,
+        strMeasure20: str | None,
+    ) -> None:
+
         self.idMeal = idMeal
         self.strMeal = strMeal
         self.strCategory = strCategory
@@ -104,20 +112,20 @@ class Recette:
         measures = []
 
         for i in range(1, 21):
-            ingredient = getattr(self, f'strIngredient{i}')
-            measure = getattr(self, f'strMeasure{i}')
+            ingredient = getattr(self, f"strIngredient{i}")
+            measure = getattr(self, f"strMeasure{i}")
             if ingredient and measure:
-                ingredients.append(f'{measure} {ingredient}')
+                ingredients.append(f"{measure} {ingredient}")
             elif ingredient:
                 ingredients.append(ingredient)
-        
-        ingredients_str = '\n'.join(ingredients)
 
-        return (f'Recette: {self.strMeal}\n'
-                f'Catégorie: {self.strCategory}\n'
-                f'Région: {self.strArea}\n'
-                f'Instructions: {self.strInstructions}\n'
-                f'Ingrédients:\n{ingredients_str}\n'
-                f'Tags: {self.strTags if self.strTags else "Aucun"}')
+        ingredients_str = "\n".join(ingredients)
 
-
+        return (
+            f"Recette: {self.strMeal}\n"
+            f"Catégorie: {self.strCategory}\n"
+            f"Région: {self.strArea}\n"
+            f"Instructions: {self.strInstructions}\n"
+            f"Ingrédients:\n{ingredients_str}\n"
+            f'Tags: {self.strTags if self.strTags else "Aucun"}'
+        )
