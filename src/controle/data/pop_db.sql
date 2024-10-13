43488b2f8935a33d59b27e8ab2e25320cb4b0ba7
xlecureuil <braquaval.xavier@gmail.com>
2024-10-13 17:49:50 +0200

    réorganisation dossier

--- src/controle/data/pop_db.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO joueur(pseudo, mdp, age, mail, fan_pokemon) VALUES
-('admin',      '0000',     0,       'admin@projet.fr',      null),
-('a',             'a',     20,      'a@ensai.fr',           true),
-('maurice',    '1234',     20,      'maurice@ensai.fr',     true),
-('batricia',   '9876',     25,      'bat@projet.fr',        false),
-('miguel',     'abcd',     23,      'miguel@projet.fr',     true),
-('gilbert',    'toto',     21,      'gilbert@projet.fr',    false),
-('junior',     'aaaa',     15,      'junior@projet.fr',     true);
