51d6e4cbea8ebcf5a8af5560688e0246922f7268
romain.w <romain.winckell@gmail.com>
2024-11-15 17:00:15 +0100

    derniere modif note moyenne

--- src/service/service_recette.py
+++ src/service/service_recette.py
@@ -119,7 +119,6 @@ class ServiceRecette:
         Affiche une recette formatée par son ID.
         """
         note = self.recette_dao.somme_note_by_recette(recette_id)
-        print(note)
         recette = self.recette_dao.update_by_recette_id(recette_id, note_moyenne=note)
         recette = self.recette_dao.get_recette_by_id(recette_id)
         if recette:
